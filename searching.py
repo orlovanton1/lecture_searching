8525c82a284399b4a7bdca918c02b5c66a516ce6
Orlov Anton (253276) <253276@vutbr.cz>
2024-04-08 13:28:51 +0200

    third

--- searching.py
+++ searching.py
@@ -28,6 +28,23 @@ def linear_search(sequence, search_number):
             list_of_times.append(index)
     return {"position": list_of_times, "count": len(list_of_times)}
 
+def pattern_search(sequence, search_seq):
+    list_of_position = []
+    pattern_lenght = len(search_seq)
+    number_of_operations = 0
+    for position in range(len(sequence)-pattern_lenght):
+        is_same = True
+        for subins in range(len(search_seq)):
+            number_of_operations = number_of_operations + 1
+            if sequence[position+subins] == search_seq[subind]:
+                is_same = is_same and True
+            else:
+                is_same = is_same and False
+        if is_same:
+            list_of_position.append(position+pattern_lenght // 2)
+    print(number_of_operations)
+    return {"position": list_of_position, "count": len(list_of_position)}
+
 def main():
     pass
 
